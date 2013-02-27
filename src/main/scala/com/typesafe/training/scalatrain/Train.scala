e42a0e49b22049368dd9c442c663c276b8eaabe1
Martin Carolan <mcarolan@gmail.com>
2013-02-27 14:45:43 +0000

    Add departureTimes map using breakOut!

--- src/main/scala/com/typesafe/training/scalatrain/Train.scala
+++ src/main/scala/com/typesafe/training/scalatrain/Train.scala
@@ -5,6 +5,7 @@
 package com.typesafe.training.scalatrain
 
 import scala.collection.immutable.Seq
+import scala.collection.breakOut
 
 case class Train(info: TrainInfo, schedule: Seq[(Time, Station)]) {
   require(schedule.size >= 2, "schedule must contain at least two elements")
@@ -12,6 +13,16 @@ case class Train(info: TrainInfo, schedule: Seq[(Time, Station)]) {
 
   val stations: Seq[Station] =
     schedule map (_._2)
+
+  val departureTimes: Map[Station, Time] =
+    //works, but traverses twice: (schedule map (_.swap)).toMap
+    (schedule map (_.swap))(breakOut)
+    //breakOut only does 1 traversal!
+    //first method is equiv to
+    //val col1: Seq[(Station, Time)] = schedule map (_.swap)
+    //val col2: Map[Station, Time] = col1 toMap
+    //breakOut uses the buildFrom of map
+    //http://stackoverflow.com/questions/1715681/scala-2-8-breakout
 
   val backToBackStations: Seq[(Station, Station)] =
     stations zip (stations tail)
--- src/test/scala/com/typesafe/training/scalatrain/TrainSpec.scala
+++ src/test/scala/com/typesafe/training/scalatrain/TrainSpec.scala
@@ -28,6 +28,15 @@ class TrainSpec extends WordSpec with MustMatchers {
     }
   }
 
+  "departureTimes" should {
+    "be initialized correctly" in {
+      Ice724.departureTimes(Munich) must be === Time(8, 50)
+      Ice724.departureTimes(Nuremberg) must be === Time(10)
+      Ice724.departureTimes(Frankfurt) must be === Time(12, 10)
+      Ice724.departureTimes(Cologne) must be === Time(13, 39)
+    }
+  }
+
   "backToBackStations" should {
     "be intialized correctly for 724" in {
       Ice724.backToBackStations must be === Vector(Munich -> Nuremberg, Nuremberg -> Frankfurt, Frankfurt -> Cologne)
